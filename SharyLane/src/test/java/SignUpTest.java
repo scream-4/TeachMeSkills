9bc6c7344ec48bd2e9ec2d7eae9cea0cd78c61d2
Stefan <stefanl95@mail.ru>
2022-07-26 12:58:23 +0300

    Les 4 and 6

--- SharyLane/src/test/java/SignUpTest.java
+++ SharyLane/src/test/java/SignUpTest.java
@@ -3,156 +3,96 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class SignUpTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void SetUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
     private static final String baseUrl = "https://www.sharelane.com/cgi-bin/register.py";
 
-    @Test
-    public void FiveDigitsZipCodeTest() {
-        //Open Chrome browser
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+    private void SendZipCode(String zipCode) {
         //Open url code zipcode page
         driver.get(baseUrl);
         //Input 5 digits into input field
-        driver.findElement(By.name("zip_code")).sendKeys("90011");
+        driver.findElement(By.name("zip_code")).sendKeys(zipCode);
         //click continue
         driver.findElement(By.cssSelector("[value=Continue]")).click();
+    }
+
+    private void Registration(String pass, String confirmPass) {
+        driver.findElement(By.name("first_name")).sendKeys("stef");
+        driver.findElement(By.name("last_name")).sendKeys("lilk");
+        driver.findElement(By.name("email")).sendKeys("stefl1234@mail.ru");
+        driver.findElement(By.name("password1")).sendKeys(pass);
+        driver.findElement(By.name("password2")).sendKeys(confirmPass);
+        driver.findElement(By.cssSelector("[value=Register]")).click();
+    }
+
+    @Test
+    public void FiveDigitsZipCodeTest() {
+        SendZipCode("90011");
         //add check
         WebElement registerButton = driver.findElement(By.cssSelector("[value=Register]"));
         Assert.assertTrue(registerButton.isDisplayed(), "'Register' button isn't displayed");
-        //close driver
-        driver.quit();
-
     }
 
     @Test
     public void FourDigitsZipCodeTest() {
-        //Open Chrome browser
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        //Open url code zipcode page
-        driver.get(baseUrl);
-        //Input 5 digits into input field
-        driver.findElement(By.name("zip_code")).sendKeys("9001");
-        //click continue
-        driver.findElement(By.cssSelector("[value=Continue]")).click();
+        SendZipCode("9001");
         //add check
         WebElement errorMessage = driver.findElement(By.className("error_message"));
         boolean isErrorDisplayed = errorMessage.isDisplayed();
-        driver.quit();
         Assert.assertTrue(isErrorDisplayed, "'Zip Code must be 5 characters");
 
     }
 
     @Test
     public void SixDigitsZipCodeTest() {
-        //Open Chrome browser
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        //Open url code zipcode page
-        driver.get(baseUrl);
-        //Input 5 digits into input field
-        driver.findElement(By.name("zip_code")).sendKeys("900111");
-        //click continue
-        driver.findElement(By.cssSelector("[value=Continue]")).click();
+        SendZipCode("900111");
         //add check
         WebElement errorMessage = driver.findElement(By.className("error_message"));
         boolean isErrorDisplayed = errorMessage.isDisplayed();
-        driver.quit();
         Assert.assertTrue(isErrorDisplayed, "'Zip Code must be 5 characters");
     }
 
     @Test
     public void PasswordThreeDigits() {
-        //Open Chrome browser
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        //Open url code zipcode page
-        driver.get(baseUrl);
-        //Input 5 digits into input field
-        driver.findElement(By.name("zip_code")).sendKeys("90011");
-        //click continue
-        driver.findElement(By.cssSelector("[value=Continue]")).click();
-        driver.findElement(By.name("first_name")).sendKeys("stef");
-        driver.findElement(By.name("last_name")).sendKeys("lilk");
-        driver.findElement(By.name("email")).sendKeys("stefl1234@mail.ru");
-        driver.findElement(By.name("password1")).sendKeys("123");
-        driver.findElement(By.name("password2")).sendKeys("123");
-        //click continue
-        driver.findElement(By.cssSelector("[value=Register]")).click();
+        SendZipCode("90011");
+        Registration("123", "123");
         //add check
         WebElement errorMessage = driver.findElement(By.className("error_message"));
         boolean isErrorDisplayed = errorMessage.isDisplayed();
-        driver.quit();
         Assert.assertTrue(isErrorDisplayed, "'Password cannot be 3 characters");
     }
 
     @Test
     public void DifferentPassword() {
-        //Open Chrome browser
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        //Open url code zipcode page
-        driver.get(baseUrl);
-        //Input 5 digits into input field
-        driver.findElement(By.name("zip_code")).sendKeys("90011");
-        //click continue
-        driver.findElement(By.cssSelector("[value=Continue]")).click();
-        driver.findElement(By.name("first_name")).sendKeys("stef");
-        driver.findElement(By.name("last_name")).sendKeys("lilk");
-        driver.findElement(By.name("email")).sendKeys("stefl1234@mail.ru");
-        driver.findElement(By.name("password1")).sendKeys("123456");
-        driver.findElement(By.name("password2")).sendKeys("1234");
-        //click continue
-        driver.findElement(By.cssSelector("[value=Register]")).click();
+        SendZipCode("90011");
+        Registration("1234", "123456");
         //add check
         WebElement errorMessage = driver.findElement(By.className("error_message"));
         boolean isErrorDisplayed = errorMessage.isDisplayed();
-        driver.quit();
         Assert.assertTrue(isErrorDisplayed, "'Password cannot be 6 characters");
     }
 
     @Test
-    public void CharactersInThePassword(){
-        //Open Chrome browser
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        //Open url code zipcode page
-        driver.get(baseUrl);
-        //Input 5 digits into input field
-        driver.findElement(By.name("zip_code")).sendKeys("90011");
-        //click continue
-        driver.findElement(By.cssSelector("[value=Continue]")).click();
-        driver.findElement(By.name("first_name")).sendKeys("stef");
-        driver.findElement(By.name("last_name")).sendKeys("lilk");
-        driver.findElement(By.name("email")).sendKeys("stefl1234@mail.ru");
-        driver.findElement(By.name("password1")).sendKeys("qazwsx");
-        driver.findElement(By.name("password2")).sendKeys("qazwsx");
-        //click continue
-        driver.findElement(By.cssSelector("[value=Register]")).click();
+    public void CharactersInThePassword() {
+        SendZipCode("90011");
+        Registration("qazwsx", "qazwsx");
     }
 
     @Test
     public void LoginSystemAndAddToCart() {
-        //Open Chrome browser
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        //Open url code zipcode page
-        driver.get(baseUrl);
-        //Input 5 digits into input field
-        driver.findElement(By.name("zip_code")).sendKeys("90011");
-        //click continue
-        driver.findElement(By.cssSelector("[value=Continue]")).click();
-        //registration
-        driver.findElement(By.name("first_name")).sendKeys("stef");
-        driver.findElement(By.name("last_name")).sendKeys("lilk");
-        driver.findElement(By.name("email")).sendKeys("stefl1234@mail.ru");
-        driver.findElement(By.name("password1")).sendKeys("1234");
-        driver.findElement(By.name("password2")).sendKeys("1234");
-        //click continue
-        driver.findElement(By.cssSelector("[value=Register]")).click();
+        SendZipCode("90011");
+        Registration("1234","1234");
         //save log in
         User user = new User();
         user.setEmail(driver.findElement(By.xpath("//td[text()='Email']/following-sibling::td")).getText());
@@ -170,6 +110,11 @@ public class SignUpTest {
         driver.get("https://www.sharelane.com/cgi-bin/add_to_cart.py?book_id=8");
         //click to Shopping cart
         driver.get("https://www.sharelane.com/cgi-bin/shopping_cart.py");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
     }
 }
 
